a4f45a26892abcef8454503ff0bc0a607556bbee
yaskyj <yaskyj@yahoo.com>
2018-01-03 20:59:48 -0600

    Begin cnn script

---
+++ CNN.py
@@ -0,0 +1,71 @@
+# Convolutional Neural Network
+
+# Importing the Keras libraries and packages
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+# Initialising the CNN
+classifier = Sequential()
+
+# Convolution
+classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+
+# Pooling
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Adding a second convolutional layer
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+# Flattening
+classifier.add(Flatten())
+
+# Full connection
+classifier.add(Dense(units = 128, activation = 'relu'))
+classifier.add(Dense(units = 1, activation = 'sigmoid'))
+
+# Compiling the CNN
+classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+
+
+from keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+
+training_set = train_datagen.flow_from_directory('train/train',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'categorical')
+
+test_set = test_datagen.flow_from_directory('train/test',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'categorical')
+
+classifier.fit_generator(training_set,
+                         samples_per_epoch = 56,
+                         nb_epoch = 25,
+                         validation_data = test_set,
+                         nb_val_samples = 10)
+
+from keras.preprocessing import image as image_utils
+import numpy as np
+ 
+test_image = image_utils.load_img('dataset/new_images/new_picture.jpg', target_size=(64, 64))
+test_image = image_utils.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis=0)
+ 
+result = classifier.predict_on_batch(test_image)
